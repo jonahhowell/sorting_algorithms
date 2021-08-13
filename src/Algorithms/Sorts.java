7e6738d67c7867a92981a4645512c0b0ed64f769
Jonah Howell <jonahowell800@gmail.com>
2021-08-13 16:02:01 -0400

    Finished heapify and buildMaxHeap. Put notes under existing sorts.

--- src/Algorithms/Sorts.java
+++ src/Algorithms/Sorts.java
@@ -3,6 +3,10 @@ package Algorithms;
 public class Sorts {
 
     public static void insertionSort(int[] nums) {
+        // Runs in O(N^2) on average
+        // O(N) best case on sorted list implementation is more emergent here than
+        // in bubble sort.
+
         // 1: For loop iterates to new, unsorted element; prior elements are sorted.
         for (int i = 0; i < nums.length; i++) {
         // 2: int j serves as a pointer element for the unsorted element.
@@ -19,6 +23,9 @@ public class Sorts {
     }
 
     public static void bubbleSort(int[] nums) {
+        // Runs in O(N^2) on average.
+
+
         int end = nums.length - 1;
         int temp;
         // 1: End is a valid index that points to the place where the next biggest element goes.
@@ -40,7 +47,7 @@ public class Sorts {
         // Allows for O(N) time on an already sorted list.
         int end = nums.length - 1;
         int temp;
-        boolean hasSwapped = false;
+        boolean hasSwapped;
         // 1: End is a valid index that points to the place where the next biggest element goes.
         while (end > 0) {
             // 2: Swaps elements if current is bigger than next.
@@ -74,5 +81,55 @@ public class Sorts {
                 nums[i] = temp;
             }
         }
+    }
+
+    public static void heapify(int[] nums, int index, int range) {
+        // Brings heap order to int current.
+        // Will only consider items in range where range = nums.length is all items.
+        int current = index;
+        int swap;
+        int temp;
+        while ((current * 2) + 1 < range) {
+            swap = current * 2 + 1;
+            if (swap + 1 < range) {
+                if ((nums[swap] > nums[current]) && (nums[swap] >= nums[swap + 1])) {
+                    temp = nums[swap];
+                    nums[swap] = nums[current];
+                    nums[current] = temp;
+                    current = swap;
+                }
+                else if ((nums[swap + 1] > nums[current]) && (nums[swap + 1] >= nums[swap])) {
+                    swap += 1;
+                    temp = nums[swap];
+                    nums[swap] = nums[current];
+                    nums[current] = temp;
+                    current = swap;
+                }
+                else break;
+            }
+            else {
+                if (nums[current] < nums[swap]) {
+                    temp = nums[current];
+                    nums[current] = nums[swap];
+                    nums[swap] = temp;
+                    current = swap;
+                }
+                else break;
+            }
+        }
+    }
+
+    public static void buildMaxHeap(int [] nums) {
+        // Builds a max heap out of an unsorted array. Used to prime an array for heap sort.
+        int current = (nums.length - 2) / 2;
+        if (current == -1) return;
+        while (current >= 0) {
+            heapify(nums, current, nums.length);
+            current -= 1;
+        }
+    }
+
+    public static void heapSort(int[] nums) {
+        // 
     }
 }
